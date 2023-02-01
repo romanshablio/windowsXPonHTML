40be8ba2c6f0de85d4c677f06d6b01d2465fe25e
romanshablio <romanshablio@icloud.com>
2023-02-01 16:40:04 +0300

    added game

--- js/start_menu.js
+++ js/start_menu.js
@@ -3,18 +3,29 @@ let startMenu = document.getElementById("startMenu");
 let myComputer = document.getElementById("myComputer");
 let myComputerWindow = document.getElementById("computerWindow");
 let windowTop = document.getElementById("myComputerPanel");
-let closeWindowButton = document.getElementById("clodeMyComputerButton");
-
+let closeWindowButton = document.getElementById("closeMyComputerButton");
+let myGamesFolder = document.getElementById("myGames");
+let gamesWindow = document.getElementById("gamesWindow");
+let closeGames = document.getElementById("closeGamesFolderButton");
+//обработчик кнопки открытия меню пуск
 startButton.addEventListener("click", function () {
   startMenu.classList.toggle("is-active");
 });
-
+// обработчик открытия папки мой компьютер
 myComputer.addEventListener("click", function () {
   myComputerWindow.classList.add("active");
   windowTop.classList.add("active");
 });
-
+// обработчик закрытия окна мой компьютер
 closeWindowButton.addEventListener("click", function () {
   myComputerWindow.classList.remove("active");
   windowTop.classList.remove("active");
+});
+
+myGamesFolder.addEventListener("click", function () {
+  gamesWindow.classList.add("active");
+});
+
+closeGames.addEventListener("click", function () {
+  gamesWindow.classList.remove("active");
 });
